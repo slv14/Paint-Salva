6da406591f89250281788bd50a5b22da983010bd
Salva007 <103088037+slv14@users.noreply.github.com>
2023-06-01 18:58:59 -0700

    archivo de ayuda

---
+++ help.py
@@ -0,0 +1,19 @@
+def imprimir_help():
+	print("""
+
+
+			Lista de comandos:
+
+			1) draw_pendiente 100,200 500,400
+			2) draw_pendiente 600,50 100,150
+			3) borrar_ultimo_trazo
+			4) aumentar_grosor 1
+			5) dibujar_circulo 600 400 100
+			6) dibujar_cuadrado 100 300 200
+			7) dibujar_triangulo_equilatero 400 200 100
+			8) dibujar_triangulo_escaleno 500 300 600 200 700 400
+			9) dibujar_triangulo_isosceles 200 200 300 300 150
+			10) color ls
+    """)
+
+imprimir_help()
